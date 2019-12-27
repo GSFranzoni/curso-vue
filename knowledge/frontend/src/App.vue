9c10dd1540447cc0168526689c623e15e8255bac
GSFranzoni <guilherme.sampaio.franzoni@gmail.com>
2019-12-27 18:51:13 -0300

    -

---
+++ knowledge/frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <Header :hideToggle='false' title='Cod3r - Base de conhecimento'></Header>
+        <Menu></Menu>
+        <Content></Content>
+        <Footer></Footer>
+        <!-- <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+        <router-view />-->
+    </div>
+</template>
+
+<script>
+import Header from "@/components/template/Header";
+import Menu from "@/components/template/Menu";
+import Content from "@/components/template/Content";
+import Footer from "@/components/template/Footer";
+
+export default {
+    name: "App",
+    components: {
+        Header,
+        Menu, 
+        Footer,
+        Content
+    }
+};
+</script>
+
+<style>
+body {
+    margin: 0;
+}
+#app {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100vh;
+
+    display: grid;
+    grid-template-areas: "menu header" "menu content" "menu footer";
+    grid-template-rows: 60px 1fr 40px;
+    grid-auto-columns: 300px 1fr;
+}
+</style>
---
+++ knowledge/frontend/src/components/template/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+      <a class="toggle" @click='toggleMenu' v-if='!hideToggle'>
+          <i class="fa fa-lg" :class='icon'></i>
+      </a>
+      <h1 class="title">
+          {{ title }}
+      </h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        hideToggle: Boolean
+    },
+    computed: {
+        icon() {
+            return "fa-angle-left";
+        }
+    },
+    methods: {
+        toggleMenu() {
+        }
+    }
+}
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        background: linear-gradient(to right, #1e469a, #49a7c1);
+
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title {
+        font-size: 1.2rem;
+        color: white;
+        text-align: center;
+        font-weight: 100;
+    }
+</style>
